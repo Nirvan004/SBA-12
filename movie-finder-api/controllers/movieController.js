650eff6244aea8b8619f84c70706aaf045c46ff1
Nirvan004 <Nirvanandb2004@gmail.com>
2026-01-13 14:22:49 -0500

    created search movies function

--- movie-finder-api/controllers/movieController.js
+++ movie-finder-api/controllers/movieController.js
@@ -1,7 +1,29 @@
 const axios = require('axios');
 
 const searchMovies = async (req, res) => {
-  
+  const { title } = req.query;
+
+  if (!title) {
+    return res.status(400).json({
+      error: 'Title query parameter is required'
+    });
+  }
+
+  try {
+    const response = await axios.get('http://www.omdbapi.com/', {
+      params: {
+        s: title,
+        apikey: process.env.OMDB_API_KEY
+      }
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      error: 'Failed to fetch movies'
+    });
+  }
 };
 
 const getMovieDetails = async (req, res) => {
