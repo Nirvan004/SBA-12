b016cc99dc031bf1b94872759ac164414fe17441
Nirvan004 <Nirvanandb2004@gmail.com>
2026-01-13 14:14:59 -0500

    created skeleton code for movie controllers file

---
+++ movie-finder-api/controllers/movieController.js
@@ -0,0 +1,14 @@
+const axios = require('axios');
+
+const searchMovies = async (req, res) => {
+  
+};
+
+const getMovieDetails = async (req, res) => {
+  
+};
+
+module.exports = {
+  searchMovies,
+  getMovieDetails
+};
